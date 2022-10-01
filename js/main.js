6a762c8d5b37415f992cdc92c01a7e59b11b055f
djalolovv <axellpubgm6@gmail.com>
2022-10-01 14:28:38 +0500

    about

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-const modal = document.querySelector('.navbar__mobile-list');
-const body = document.querySelector('body');
-const btn = document.querySelector('.navbar__btn');
+const modal = document.querySelector(".navbar__mobil-list");
+const body = document.querySelector("body");
+const btn = document.querySelector(".navbar__btn");
 
-btn.addEventListener("click",()=> {
-  modal.classList.toggle('open');
-  body.classList.toggle('overflow')
-})
+btn.addEventListener("click", () => {
+  modal.classList.toggle("open");
+  body.classList.toggle("overflow");
+  console.log("sefefwef");
+});
